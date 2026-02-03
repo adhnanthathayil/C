9a8c8d09857167966b7cee4e6874489c5d56190e
adhnanthathayil <adhnanthathayil@gmail.com>
2026-02-04 00:48:41 +0530

    Add files via upload

---
+++ 5.1b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if (n%2== 0)
+        printf("NUMBER IS EVEN\n");
+    else
+        printf("NUMBER IS ODD\n");
+
+    return 0;
+}
