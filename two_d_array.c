dbdd5e01281b0d0aabd74c0dd6981d2518e90ecf
adhnanthathayil <adhnanthathayil@gmail.com>
2026-02-10 10:03:06 +0530

    Fix input and output for 2D array program

--- two_d_array.c
+++ two_d_array.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    int a[100][100],i,j,n,m;
-    printf("Enter row size & column size");
-    scanf("%d%d",&m,&n);// Write C code here
-    printf("Enter matrix size");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;i<<n;j++)
-        {
-            printf("%d",&a[i][j]);
+int main(){
+    int a[100][100],i,j,m,n;
+
+    printf("Enter row size & column size:");
+    scanf("%d%d",&m,&n);
+
+    printf("Enter matrix elements:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
         }
+    }
+
+    printf("Matrix is:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
